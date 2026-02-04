ee1e2b6b73d73abf5d57101c4e72daf31bdb6dd6
AlexCh <alex.shaginyan0507@gmail.com>
2026-02-04 21:35:16 +0100

    Creation du User/Registration

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
@@ -26,4 +27,56 @@ class MailController extends AbstractController
 
         return new Response('Email envoyé !');
     }
+
+    #[Route('/send-mail-html', name: 'app_send_mail_html')]
+    public function sendMailHtml(MailerInterface $mailer): Response
+    {
+        $username = 'John Doe';
+
+//        return $this->render('mail/index.html.twig');
+        $email = (new TemplatedEmail())
+            ->from('admin@phonstore.com')
+            ->to('client@phonestore.com')
+            ->subject('Confirmation de création de votre compte PhoneStore !')
+            ->htmlTemplate('email/new_mail.html.twig')
+            ->context([
+                'username' => $username
+            ]);
+
+        $mailer->send($email);
+
+        return new Response('Email HTML envoyé avec succès !');
+    }
+
+
+    #[Route('/send-order-confirmation', name: 'app_send_order_confirmation')]
+    public function sendOrderConfirmation(MailerInterface $mailer): Response
+    {
+        $username = 'John Doe';
+
+        $order = [
+            'number' => 'CMD-2026-045',
+            'date' => new \DateTimeImmutable(),
+            'total' => 899.99,
+            'items' => [
+                ['name' => 'iPhone 15', 'quantity' => 1, 'price' => 799.99],
+                ['name' => 'Coque silicone', 'quantity' => 1, 'price' => 100.00],
+            ],
+        ];
+
+        $email = (new TemplatedEmail())
+            ->from('commande@phonestore.com') // expéditeur du mail
+            ->to('client@phonestore.com') // destinataire du mail
+            ->subject('Confirmation de votre commande PhoneStore !') // sujet du mail
+            ->htmlTemplate('email/order_confirmation.html.twig') // contenu du mail
+            ->context([
+                'username' => $username,
+                'order' => $order,
+            ]);
+
+        $mailer->send($email);
+
+        return new Response('Mail de confirmation de commande envoyé avec succès !');
+    }
+
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,6 +11,36 @@ class ProductController extends AbstractController
     #[Route('produits', name: 'app_produits')]
     public function produits(): Response
     {
-        return $this->render('product/produits.html.twig');
+        $products = [
+            [
+                'id' => 1,
+                'name' => 'iPhone 13 Pro',
+                'image' => 'iphone-13-pro.jpg',
+                'price' => 999.00,
+                'brand' => 'Apple',
+                'model' => 'iPhone 13'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Samsung Galaxy S21',
+                'image' => 'samsung-s21.jpg',
+                'price' => 799.00,
+                'brand' => 'Samsung',
+                'model' => 'Galaxy S21'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Google Pixel 6a',
+                'image' => 'pixel-6a.jpg',
+                'price' => 459.00,
+                'brand' => 'Google',
+                'model' => 'Pixel 6a'
+            ],];
+
+        return $this->render('product/produits.html.twig', [
+            'products' => $products
+        ]);
     }
+
+
 }
